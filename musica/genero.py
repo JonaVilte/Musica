f8e75c8803947bfbbc1d9cee17bc2dc98638e919
Jonatan <togo36918@gmail.com>
2024-07-01 12:07:42 -0300

    Se modificaron codigos en detalle_canciones, genero.html y se eliminiaron cosas en genero.py y en detalle_canciones

--- musica/genero.py
+++ musica/genero.py
@@ -7,8 +7,8 @@ bp = Blueprint('genero', __name__, url_prefix='/genero')
 def generos():
     base_de_datos = db.get_db()
     consulta = """
-        SELECT name, genreId FROM genres
-        ORDER BY genreId ASC	 
+        SELECT name, GenreId FROM genres
+        ORDER BY GenreId ASC	 
         """
     resultado = base_de_datos.execute(consulta)
     lista_de_resultados = resultado.fetchall()
@@ -19,21 +19,16 @@ def generos():
 def detalle(id):
     base_de_datos = db.get_db()
     consulta = """
-        SELECT name FROM genres 
-        WHERE genreid = ?		 
+        SELECT  t.name as nombre, t.GenreId FROM genres t
+		JOIN tracks g on t.GenreId=g.GenreId	
+        WHERE t.GenreId = ?		 
         """
 
-    consulta2 = """
-        SELECT t.name, t.genreId from genres t
-        JOIN tracks c on t.GenreId = c.GenreId
-        WHERE t.GenreId	= ?
-        """
+
 
     resultado = base_de_datos.execute(consulta, (id,))
-    genero = resultado.fetchone()
-    resultado = base_de_datos.execute(consulta2, (id,))
-    lista_de_resultados = resultado.fetchall()
+    generos = resultado.fetchone()
+
     pagina = render_template("detalle_genero.html", 
-                                        genero=genero,
-                                        canciones=lista_de_resultados)
+                                        genero=generos)
     return pagina
